2a9e46e3fad4032ae72d5c241839ca7d2116c033
dark <dark@DESKTOP-F0STRUP>
2020-12-20 20:39:16 +0200

    Добавьте файлы проекта.

---
+++ ConsoleAppEF/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsoleAppEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid MyProperty { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class AppContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=testDb; Trusted_Connection=True");
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
